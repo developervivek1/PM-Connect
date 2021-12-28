5e739a5db49e7d0e0423be2e573d9dba683df3dd
developervivek1 <vivekpathak.emmy1234@gmail.com>
2021-12-28 15:36:17 +0530

    project--list

---
+++ emplyee-end-user/js/menuscript.js
@@ -0,0 +1,30 @@
+const overlay = document.querySelector('.myoverlay');
+const mobileTrigger = document.querySelector('.mobile-menuhead .hamberger-menu span');
+const mobileMenu = document.querySelector('nav');
+
+mobileTrigger.addEventListener('click',()=>
+{
+    if(!mobileMenu.classList.contains('active'))
+    {
+       mobileMenu.classList.add('active');
+       overlay.classList.add('active');
+    }
+})
+
+window.addEventListener('click', hideFilterdiv);
+function hideFilterdiv(e) {
+    if (e.type === "click" && e.target == overlay) {
+        mobileMenu.classList.remove('active');
+        overlay.classList.remove('active');
+        // console.log('hello');
+    }
+}
+
+window.addEventListener('resize',()=>
+{
+    if(this.innerWidth < 849)
+    {
+       overlay.classList.remove('active');
+       mobileMenu.classList.remove('active');
+    }
+})
