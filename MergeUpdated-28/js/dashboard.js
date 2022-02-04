8c7da1f4029871e2f7df854013db5c0eab83de3b
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-02-04 16:17:28 +0530

    dashboard  redesign --2

--- MergeUpdated-28/js/dashboard.js
+++ MergeUpdated-28/js/dashboard.js
@@ -72,6 +72,50 @@ $(document).ready(function () {
     });
 });
 
+
+$(document).ready(function () {
+  
+    $('.chart.doughnut').each(function () {
+        type: 'doughnut';
+        var chartID = $(this).find('canvas').attr('id');
+        var c = document.getElementById(chartID);
+        var width = $('#' + chartID).width();
+        var height = width;
+        var linewidth = width / 8;
+        c.setAttribute('width', width * 2);
+        c.setAttribute('height', height * 2);
+        var ctx = c.getContext('2d');
+        var startingPoint =-0.5;
+        ctx.scale(2, 2);
+        ctx.beginPath();
+        ctx.arc(height / 2, height / 2, width / 3, 0 * Math.PI, 1 * Math.PI);
+        ctx.lineWidth = linewidth;
+      //  ctx.strokeStyle = 'none';
+        //ctx.shadowOffsetX = 3;
+       //ctx.shadowOffsetY = 3;
+      //  ctx.shadowBlur = 20;
+       // ctx.shadowColor = "rgba(0,0,0,0.2)";
+        ctx.stroke();
+
+        $(this).find('li').each(function () {
+
+            var start = startingPoint;
+            var dataValue = $(this).find('data').attr('value');
+            var end = startingPoint + (2.5 * dataValue);
+            var color = $(this).find('data').data('color');
+            ctx.beginPath();
+            ctx.arc(height / 2, height / 2, width / 3, startingPoint * Math.PI, end * Math.PI);
+            ctx.lineWidth = linewidth;
+            ctx.strokeStyle = color;
+            ctx.stroke();
+            $(this).css('color', color);
+        });
+    });
+});
+
+
+
+
 const getinput = document.querySelector('.searchbox input');
 const highlight = document.querySelector('.searchdiv .card');
 
